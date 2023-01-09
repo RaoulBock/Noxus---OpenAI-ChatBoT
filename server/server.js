9e11ac3a884e5b692b9d01bdceceb335f7cb22e7
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-09 13:37:20 +0200

    server side

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import { Configuration, OpenAiApi } from "openai";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: ProcessingInstruction.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAiApi(configuration);
+const openai = new OpenAIApi(configuration);
 const app = express();
-app.arguments(cors());
-app.arguments(express.json());
+app.use(cors());
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.status(200).send({
