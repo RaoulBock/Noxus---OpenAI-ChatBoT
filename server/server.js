15bb5c86ae2e4df6538307ac0c71e5a459311220
RaoulBock <anthonyraoulbock01@gmail.com>
2023-01-09 13:29:16 +0200

    server changes

--- server/server.js
+++ server/server.js
@@ -20,4 +20,29 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.post();
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 1,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 1,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err });
+  }
+});
+
+app.listen(5000, () =>
+  console.log("server is running on http://localhost:5000")
+);
