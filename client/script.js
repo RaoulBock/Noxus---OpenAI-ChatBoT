f637e1576ed8b2f0fb1f39d16fc51c99c07037df
RaoulBock <anthonyraoulbock01@gmail.com>
2023-02-08 15:58:53 +0200

    front-end

--- client/script.js
+++ client/script.js
@@ -88,11 +88,11 @@ const handleSubmit = async (e) => {
   const response = await fetch("http://localhost:5000/", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      prompt: data.get("prompt"),
-    }),
+      prompt: data.get("prompt")
+    })
   });
 
   clearInterval(loadInterval);
